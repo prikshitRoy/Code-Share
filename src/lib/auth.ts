8e3d6cb4fdbcbc855cd9da67fe16e1708f5ff839
parikshitRoy <parikshitroy00@gmail.com>
2024-04-16 17:45:57 +0530

    Build Error `Fixed`

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 import {
   GetServerSidePropsContext,
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -28,4 +28,6 @@ export const authOptions: NextAuthOptions = {
 
 const handler = NextAuth(authOptions);
 
+export default handler;
+
 export { handler as GET, handler as POST };
