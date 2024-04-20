f2ab648a4bececebdb6093147241acbeaf165da2
parikshitRoy <parikshitroy00@gmail.com>
2024-04-20 18:57:52 +0530

    Header component completed

--- src/components/Header/Home.tsx
+++ src/components/Header/Home.tsx
@@ -1,9 +1,41 @@
 "use client";
 
+import Kbd from "../ui/Kbd";
+import { cn } from "@/lib/cn";
+
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+
 export default function Home() {
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <Link
+        href="/dashboard"
+        className={cn(
+          "flex items-center gap-6 rounded-lg p-1 font-medium",
+          "select-none outline-none",
+          "border border-white/20 bg-black",
+          "transition-all duration-200 ease-in-out",
+          "hover:bg-white/20 hover:text-almost-white",
+          "focus:text-almost-white focus:ring-1 focus:ring-almost-white focus:ring-offset-2 focus:ring-offset-black",
+          "group"
+        )}
+      >
+        <div className={cn("flex items-center gap-2")}>
+          <ArrowLeft
+            size={16}
+            className={cn(
+              "translate-x-0.5",
+              "transition-transform duration-100 ease-in-out",
+              "group-hover:translate-x-0"
+            )}
+            aria-hidden="true"
+          />
+          Dashboard
+        </div>
+
+        <Kbd keys={["B"]} />
+      </Link>
+    </div>
   );
 }
--- src/components/Header/Social.tsx
+++ src/components/Header/Social.tsx
@@ -1,7 +1,71 @@
+import { cn } from "@/lib/cn";
+
+import Link from "next/link";
+import Image from "next/image";
+/* 
+import { Github, LucideProps, Twitter } from "lucide-react";
+*/
+
 export default function Social() {
+  const links = [
+    {
+      id: "github",
+      href: "",
+      /*  
+      icon: (props: LucideProps) => <GitBranch {...props} aria-hidden="true" />, 
+      */
+      icon: "/github.svg",
+      label: "GitHub",
+    },
+    {
+      id: "twitter",
+      href: "",
+      /* 
+      icon: (props: LucideProps) => ( <ScanFaceIcon {...props} aria-hidden="true" /> ), 
+      */
+      icon: "/twitter.svg",
+      label: "Twitter",
+    },
+  ];
+
   return (
-    <>
-      <div></div>
-    </>
+    <nav className={cn("relative mr-5 pr-5")}>
+      <div className={cn("flex items-center gap-2")}>
+        {links.map(({ id, href, icon, label }) => (
+          <Link
+            key={id}
+            href={href}
+            target="_blank"
+            rel="noreferrer"
+            className={cn(
+              "rounded-kg p-2",
+              "select-none outline-none",
+              "transition-all duration-100 ease-in-out",
+              "hover:cursor-ne-resize hover:bg-white/20 hover:text-almost-white",
+              "focus:text-almost-white focus:outline-1 focus:outline-offset-2 focus:outline-almost-white"
+            )}
+            aria-label={`social-link-${id}`}
+          >
+            {/* 
+             {icon({ className: cn("w-[18px] h-[18px]") })}
+            */}
+            <Image
+              src={icon}
+              alt={label}
+              className={cn("w-[18px] h-[18px]")}
+              aria-hidden="true"
+            />
+            <span className="sr-only">{label}</span>
+          </Link>
+        ))}
+      </div>
+
+      <div
+        className={cn(
+          "absolute right-0 top-0 h-full w-px",
+          "bg-gradient-to-b from-transparent via-white/20 to-transparent"
+        )}
+      />
+    </nav>
   );
 }
